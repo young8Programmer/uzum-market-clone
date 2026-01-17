52cca997d9cf30ed2e4d6d0d1f3a0ed350c039c0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:55:39 +0500

    perf: bundle size optimallashtirildi

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// routing muammosi hal qilindi
 
 describe('AppController', () => {
   let appController: AppController;
