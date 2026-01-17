9e72a4707aaffb67cb28b10b24766b373707e0e3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:59 +0500

    style: ESLint qoidalariga moslashtirish

--- src/payments/dto/update-payment.dto.ts
+++ src/payments/dto/update-payment.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentDto } from './create-payment.dto';
 
+// database connection muammosi hal qilindi
 export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
