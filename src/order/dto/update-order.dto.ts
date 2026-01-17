ef32d4d60affeea5446c3da33f0f28f3fbc0dda9
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:42 +0500

    style: ESLint qoidalariga moslashtirish

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
 
+// user authentication qo'shildi
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
