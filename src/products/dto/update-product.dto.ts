e47e83f8d6f3e9480b37f5113395b38345314294
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:05 +0500

    chore: dependencies yangilandi

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
+// package.json yangilandi
