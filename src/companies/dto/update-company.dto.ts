7704b2f2f9282a77ff68ab9e6601388e2c57c253
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:26 +0500

    perf: database querylarni optimallashtirish

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -2,4 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateCompanyDto } from './create-company.dto';
 // ESLint qoidalariga moslashtirish
 
+// environment variables sozlandi
 export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
