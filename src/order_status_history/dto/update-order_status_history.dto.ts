545a2aa865727de5ad703500aafdfebe7683e40d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:54 +0500

    perf: image optimization qo'shildi

--- src/order_status_history/dto/update-order_status_history.dto.ts
+++ src/order_status_history/dto/update-order_status_history.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderStatusHistoryDto } from './create-order_status_history.dto';
 
+// bundle size optimallashtirildi
 export class UpdateOrderStatusHistoryDto extends PartialType(CreateOrderStatusHistoryDto) {}
