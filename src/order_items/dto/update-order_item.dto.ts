ff1d5d2efb345a35d3c2fef950a87f2c43d1ceb1
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:48 +0500

    chore: environment variables sozlandi

--- src/order_items/dto/update-order_item.dto.ts
+++ src/order_items/dto/update-order_item.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderItemDto } from './create-order_item.dto';
 
 export class UpdateOrderItemDto extends PartialType(CreateOrderItemDto) {}
+// package.json yangilandi
