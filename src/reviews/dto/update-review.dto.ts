203455b05f56e204e7356abae4909648dd27ddde
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:12 +0500

    style: kod uslubini yaxshilash

--- src/reviews/dto/update-review.dto.ts
+++ src/reviews/dto/update-review.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReviewDto } from './create-review.dto';
 
+// admin dashboard yaratildi
 // unit testlar qo'shildi
 export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
