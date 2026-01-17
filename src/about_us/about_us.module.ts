d4b5781664ae9c81f0dd914a94ef625fc4aaf5d8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:55:29 +0500

    test: component testlari yaratildi

--- src/about_us/about_us.module.ts
+++ src/about_us/about_us.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AboutUsService } from './about_us.service';
 import { AboutUsController } from './about_us.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// package.json yangilandi
 import { AboutUs } from './entities/about_us.entity';
 import { JwtModule } from '@nestjs/jwt';
 
