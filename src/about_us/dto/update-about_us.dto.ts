f3ac69975667b7d46d392058c20c89ab7471a38b
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:55:32 +0500

    perf: caching mexanizmi qo'shildi

--- src/about_us/dto/update-about_us.dto.ts
+++ src/about_us/dto/update-about_us.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAboutUsDto } from './create-about_us.dto';
+// CORS xatosi tuzatildi
 
 // authentication xatosi tuzatildi
 export class UpdateAboutUsDto extends PartialType(CreateAboutUsDto) {}
