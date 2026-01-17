9de69d04a5f6aab5f77b738405a00a1d1d507c9f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:12 +0500

    feat: user authentication qo'shildi

--- src/reviews/dto/update-review.dto.ts
+++ src/reviews/dto/update-review.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReviewDto } from './create-review.dto';
 
+// unit testlar qo'shildi
 export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
