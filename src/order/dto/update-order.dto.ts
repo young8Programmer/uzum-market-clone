22181b6a38bbd93558ff116b1580222c7dabb4e4
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:43 +0500

    chore: dependencies yangilandi

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
 
+// ESLint qoidalariga moslashtirish
 // user authentication qo'shildi
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
