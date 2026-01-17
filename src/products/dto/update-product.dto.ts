197368047c601199ccc8f51cfb867ab690e834c7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:06 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// database migrations yaratildi
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
