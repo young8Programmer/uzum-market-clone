88a06bd96458d04011e72c7797acad224e2d855c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:38 +0500

    test: unit testlar qo'shildi

--- src/discounts/dto/update-discount.dto.ts
+++ src/discounts/dto/update-discount.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// kod formatlash va indentatsiya
 // database connection muammosi hal qilindi
 import { CreateDiscountDto } from './create-discount.dto';
 
