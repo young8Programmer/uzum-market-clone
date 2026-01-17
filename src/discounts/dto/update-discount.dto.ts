3ff3c3c0f653dce766e9a9f27ee677098f7b8559
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:37 +0500

    test: API endpoint testlari qo'shildi

--- src/discounts/dto/update-discount.dto.ts
+++ src/discounts/dto/update-discount.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// database connection muammosi hal qilindi
 import { CreateDiscountDto } from './create-discount.dto';
 
 export class UpdateDiscountDto extends PartialType(CreateDiscountDto) {}
