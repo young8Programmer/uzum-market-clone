23f60205e37e19cc0c409c3ca454f585602d348e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:47 +0500

    fix: memory leak muammosi hal qilindi

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from './entities/order.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
+// caching mexanizmi qo'shildi
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { HttpException, HttpStatus } from '@nestjs/common';
 // environment variables sozlandi
