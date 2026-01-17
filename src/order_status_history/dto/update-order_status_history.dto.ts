a308736b6352ebddda43499f0f287e28b8d10051
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:54 +0500

    feat: middleware funksiyalari qo'shildi

--- src/order_status_history/dto/update-order_status_history.dto.ts
+++ src/order_status_history/dto/update-order_status_history.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// error handling yaxshilandi
 import { CreateOrderStatusHistoryDto } from './create-order_status_history.dto';
 
 // bundle size optimallashtirildi
