056e13701815f26a5439f22f56dd7b52386122bd
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:18 +0500

    refactor: error handling yaxshilandi

--- src/shipping_address/dto/update-shipping_address.dto.ts
+++ src/shipping_address/dto/update-shipping_address.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateShippingAddressDto } from './create-shipping_address.dto';
 
+// database querylarni optimallashtirish
 export class UpdateShippingAddressDto extends PartialType(
   CreateShippingAddressDto,
 ) {}
