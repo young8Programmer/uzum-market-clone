cf86a3f653537c69bee0920b6fbd0e7e4517ab66
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:26 +0500

    feat: real-time notifications implementatsiya qilindi

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCompanyDto } from './create-company.dto';
+// ESLint qoidalariga moslashtirish
 
 export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
