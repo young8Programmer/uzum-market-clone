bebed65703e7e6c8d9ecb236310fd9c80ef91ff0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:55:31 +0500

    feat: real-time notifications implementatsiya qilindi

--- src/about_us/dto/update-about_us.dto.ts
+++ src/about_us/dto/update-about_us.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAboutUsDto } from './create-about_us.dto';
 
+// authentication xatosi tuzatildi
 export class UpdateAboutUsDto extends PartialType(CreateAboutUsDto) {}
