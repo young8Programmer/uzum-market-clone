dc2f32038629ac10e3742358390537c7378055ff
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:46 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// bundle size optimallashtirildi
 // API endpoint testlari qo'shildi
 import { Order } from './entities/order.entity';
 import { JwtModule } from '@nestjs/jwt';
