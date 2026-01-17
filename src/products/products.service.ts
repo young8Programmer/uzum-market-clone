ab12cb4c83d76642305c2389efffa09d12da3992
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:10 +0500

    fix: validation xatolari tuzatildi

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+// build konfiguratsiyasi sozlandi
 import { Product } from './entities/product.entity';
 
 @Injectable()
