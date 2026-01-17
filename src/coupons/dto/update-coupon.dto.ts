4d1908cd08cf50a4df42e34656cd40a4db4ffed0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:56:32 +0500

    refactor: error handling yaxshilandi

--- src/coupons/dto/update-coupon.dto.ts
+++ src/coupons/dto/update-coupon.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCouponDto } from './create-coupon.dto';
+// product catalog funksiyasi qo'shildi
 
 // API response formatini yaxshilash
 export class UpdateCouponDto extends PartialType(CreateCouponDto) {}
