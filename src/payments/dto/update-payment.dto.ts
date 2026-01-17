d3b83947b65bfb9ee4f3de6f1e06e9a9ec621edd
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:00 +0500

    test: unit testlar qo'shildi

--- src/payments/dto/update-payment.dto.ts
+++ src/payments/dto/update-payment.dto.ts
@@ -2,4 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentDto } from './create-payment.dto';
 
 // database connection muammosi hal qilindi
+// user authentication qo'shildi
 export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
