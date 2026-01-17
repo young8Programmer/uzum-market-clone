e625977b8f245ac4050bd03b098f0fadc42724ad
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:57:09 +0500

    docs: code comments qo'shildi

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// package.json yangilandi
 import { Product } from './entities/product.entity';
 import { JwtModule } from '@nestjs/jwt';
 
